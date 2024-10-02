676b3254c048ef1ca4d6ff1a39526ce336a9abbe
Incog <incogcyberpunk@gmail.com>
2024-10-02 18:44:14 +0545

    Fixed the home page for mobiles

--- frontEnd/src/pages/home/home.jsx
+++ frontEnd/src/pages/home/home.jsx
@@ -2,12 +2,15 @@ import Sidebar from "../../components/sideBar/sideBar"
 import MessagesContainer from "../../components/messagesContainer/messagesContainer"
 
 const Home = () => {
+  const toggleView= () => {
+  }
+  
   return (
-    <div className="flex sm:h-[450px] md:h-[550px] overflow-auto pb-6 pl-3 pr-3 bg-gray-600  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 rounded-xl pt-4  justify-center mx-auto">
-      <div className="sideBar flex-col border-r-2 border-gray-800 pr-3 h-full">
+    <div className="flex h-[48rem] sm:h-[60rem] w-[27rem] sm:w-[55rem] overflow-auto pb-6 pl-3 pr-3 bg-gray-600  bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40 rounded-xl pt-4  justify-center mx-auto">
+      <div className="sideBar flex-col sm:border-r-2  border-gray-800 pr-3 h-full">
         <Sidebar className=""/>
       </div>
-      <div className="messages flex flex-col items-center justify-center">
+      <div className="messages hidden lg:flex flex-col items-center justify-center">
         <MessagesContainer />
       </div>
     </div>
